fb9901fd8e91d85a8af4f5ea2f6bde11a9359fdb
yachen <io260188@gmail.com>
2020-05-13 12:44:09 +0800

    feat: remove tab

--- main.js
+++ main.js
@@ -21,12 +21,14 @@ class Tab {
             this.lis.forEach((li, i) => {
                 this.lis[i].i = i;
                 this.lis[i].addEventListener('click', this.toggleTab);
+                this.remove[i].addEventListener('click', this.removeTab);
             })
         }
-        // 獲取所有小 li 與 section
+        // 需要重新獲取動態添加的元素
     updateNode() {
             this.lis = this.main.querySelectorAll('li');
             this.sections = this.main.querySelectorAll('section');
+            this.remove = this.main.querySelectorAll('.icon-remove')
         }
         // 1. 切換功能
     toggleTab() {
@@ -36,9 +38,8 @@ class Tab {
             // console.log(that.sections[this.i]);
             this.className = 'li-active';
             // console.log(this.sections[this.i])
-            // this 指向被點擊的 li，裡面沒有 sections，所以會報錯
-            // 我們要的是 constructor 裡面的 this.section，
-            // 解決方式：在全域設定變數(that)接
+            // this 指向被點擊的 li，裡面沒有 sections，會報錯
+            // 我們要的是 constructor 裡面的 this.section，解決方式：在全域設定變數(that)接
             // this.sections[this.i].className = 'content-active';
             that.sections[this.i].className = 'content-active';
 
@@ -54,7 +55,7 @@ class Tab {
     addTab() {
             that.clearClass();
             // step1: 創建 li 和 section
-            let li = '<li class="li-active"><span>新選項卡</span><span class="iconfont icon-guanbi"></span></li>';
+            let li = '<li class="li-active"><span>新選項卡</span><span class="iconfont icon-remove"></span></li>';
             let section = '<section class="content-active">gfg</section>';
             // step2: 塞入對應的容器
             // this.add 調用的，所以 this 指向 this.add，而 this.add 裡沒有 ul
@@ -64,7 +65,24 @@ class Tab {
             that.init();
         }
         // 3. 刪除功能
-    removeTab() {}
+    removeTab(e) {
+            // 阻止冒泡觸發到父層 li 的點擊事件
+            e.stopPropagation();
+            let index = this.parentNode.i;
+            console.log(index);
+            // remove() 方法可直接刪除指定的元素
+            that.lis[index].remove();
+            that.sections[index].remove();
+            // 刪除後再重新獲取元素
+            that.init();
+            // 當刪除的不是選定狀態的 li 時，原來的選中狀態保持不變
+            if (document.querySelector('.li-active')) return;
+            // 當刪除了選定狀態的 li 時，讓其前一個 li 變成選定狀態
+            index--;
+            // 無需鼠標觸發，即可調用點擊事件
+            // 短路保護機制，第一個 index (0 - 1) = -1，但 lis 最小是 0，會報錯
+            that.lis[index] && that.lis[index].click();
+        }
         // 4. 修改功能
     editTab() {}
 }
